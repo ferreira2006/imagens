f76ecec7c92125b48c3c475e45a3f65a34bcbe2b
ferreira2006 <ferreira2006@gmail.com>
2023-04-22 18:07:31 -0300

    Imagens do tutorial Hello World Spring

---
+++ imagesweb/src/main/java/com/example/images/ImageswebApplication.java
@@ -0,0 +1,13 @@
+package com.example.images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageswebApplication.class, args);
+	}
+
+}
